21fed350f2c7eeed2570c3f26574e5d6340a0d73
MaressaMotta <91914924+MaressaMotta@users.noreply.github.com>
2022-04-09 15:33:02 +0100

    trying to change background

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,7 +6,7 @@ import WeatherIcon from "./WeatherIcon";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <h1>
+      <h1 className="mt-2">
         It's{" "}
         <span className="temperature">
           {Math.round(props.data.temperature)}°C
@@ -14,22 +14,19 @@ export default function WeatherInfo(props) {
         in {props.data.city}
       </h1>
       <br />
-      
-        
-         <div className="temperature-container d-flex ">
+      <div className="temperature-container d-flex justify-content-equaly">
         <WeatherIcon code={props.data.icon} size={72} />
-      
-      <ul>
-        <li>
-          <FormattedDate date={props.data.date} />
-        </li>
-        <li className="text-capitalize">{props.data.description}</li>
-        <li>Humidity: {props.data.humidity}%</li>
-        <li>Wind: {props.data.wind} km/h</li>
-      </ul>
-</div>
-     
-      
+{""}
+        <ul>
+          <li>
+            <FormattedDate date={props.data.date} />
+          </li>
+          <li className="text-capitalize">{props.data.description}</li>
+          <li>Humidity: {props.data.humidity}%</li>
+          <li>Wind: {props.data.wind} km/h</li>
+        </ul>
+      </div>
+      <br />
       <hr />
     </div>
   );
